cc395441e0b90ae83b0a0c16a606f2f664c7596a
paulint <tamas.paulin@bvtech.io>
2019-12-07 12:44:26 +0100

    progressing in tutorials

--- src/main/kotlin/com/examle/nulls/Nulls.kt
+++ src/main/kotlin/com/examle/nulls/Nulls.kt
@@ -1,8 +1,38 @@
 package com.examle.nulls
 
-// ? sace-call operator, telling the variable can be null
+// ? means the variable can be null or type
 data class Employee(val name: String, val employees: List<Employee>?)
 
+// If not null, then call uppercase or return default value.
+// Elvis operator: ?:
+fun uppercaseStr(str: String?) = str?.toUpperCase() ?: "Was null!"
+
+fun letOperatorTest(): String? = null
+
+// Nothing = the function never terminates normally
+fun nothingTest(): Nothing = throw IllegalArgumentException()
+
 fun main(args: Array<String>) {
-//    val boss = Employee()
+    val emplOne = Employee("Tamas", null)
+    val emplTwo = Employee("Laci", listOf(emplOne))
+
+    val boss = Employee("Sanyi", null)
+
+    // ? means safe-call operator = null check + method call
+    println(boss.employees?.first())
+
+    // Safe-cast operator: cast or null
+    println(12 as? Double ?: false)
+
+    // let keyword = calls lambda only if variable not null or does nothing
+    val letTest: String? = letOperatorTest()
+    println(letTest?.let { it.toUpperCase() })
+
+    // Extension functions can be called safely on nulls.
+    val nullStr: String? = null
+    println(nullStr.isNullOrBlank())
+
+    // Convert to non-null or throw exception if null
+    val str: String? = null
+    println(str!!)
 }
--- src/test/kotlin/com/examle/HelloTest.kt
+++ src/test/kotlin/com/examle/HelloTest.kt
@@ -1,8 +1,28 @@
 package com.examle
 
+import com.examle.model.Person
+import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
-import kotlin.test.assertEquals
+import org.junit.runner.RunWith
+import org.mockito.runners.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class HelloTest {
 
+    // For DI FWs mostly like Spring.
+    private lateinit var person: Person
+
+    @Before
+    fun setup() {
+        person = Person("Tamas", 13)
+    }
+
+    @Test
+    fun testMockitoWithKotlin() {
+        // when
+        var actualAge = person.age
+        // then
+        assertEquals(13, actualAge)
+    }
 }
