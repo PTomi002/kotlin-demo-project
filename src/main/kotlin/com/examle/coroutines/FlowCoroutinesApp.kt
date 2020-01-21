2dfbb6effc7c37bf015e8ba2c6f812e986a5ce43
paulin1tama929 <paulin.tamas@telekom.hu>
2020-01-21 11:57:00 +0100

    Add kotlin coroutines tutorials

--- src/main/kotlin/com/examle/coroutines/FlowCoroutinesApp.kt
+++ src/main/kotlin/com/examle/coroutines/FlowCoroutinesApp.kt
@@ -1,8 +1,14 @@
 package com.examle.coroutines
 
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() {
@@ -21,5 +27,55 @@ fun main() {
 
     // Reactive Channel between coroutines
     val channel = Channel<Int>()
+    runBlocking {
+        println("Launch channel")
+        launch {
+            for (i in 1..5) {
+                channel.send(i)
+            }
+            channel.close()
+        }
+        for (i in channel) {
+            println("Channel: $i")
+        }
+    }
 
+    // Same with producer/consumer pattern
+    runBlocking {
+        val producer = produce {
+            for (i in 1..5) {
+                send(i)
+            }
+        }
+        producer.consumeEach {
+            println("Producer/Consumer Channel: $it")
+        }
+    }
+
+    // Fan-Out - Distribute workload
+    runBlocking {
+        val producer = producer(this)
+        repeat(5) {
+            processor(this@runBlocking, it, producer)
+        }
+        delay(1000)
+        // cancelling a producer coroutine closes its channel
+        producer.cancel()
+    }
 }
+
+fun producer(coroutineScope: CoroutineScope) = coroutineScope.produce {
+    var counter = 0
+    while (true) {
+        send(counter++)
+        delay(100)
+    }
+}
+
+fun <T> processor(coroutineScope: CoroutineScope, id: Int, receiveChannel: ReceiveChannel<T>) =
+    coroutineScope.launch {
+        // for loop pattern is perfectly safe to use from multiple coroutines
+        for (i in receiveChannel) {
+            println("Received via channel: $i in thread: ${Thread.currentThread().name} in: $id")
+        }
+    }
